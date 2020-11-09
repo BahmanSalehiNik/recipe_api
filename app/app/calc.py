528a058e544db80678ea03e62f2b9103670e637f
salehi <b.salehi@kardan.ir>
2020-11-09 18:46:31 +0330

    sets local pycharm

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    """
+    :param a: a number
+    :param b: a number
+    :return: sum of a and b
+    """
+    return a+b
+
---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from app.calc import add
+
+
+class CalcTest(TestCase):
+
+    def test_add_numbers(self):
+        """
+        Test that two numbers asre added together
+        """
+        self.assertEqual(add(3, 5), 8)
